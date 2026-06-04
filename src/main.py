b7d40a929d71c5262dbad43d7cbb4834ee2039b2
Lysandre B. <lysandre.boursette@epitech.eu>
2026-06-04 17:09:22 +0200

    chore: Removed useless code

--- src/main.py
+++ src/main.py
@@ -4,22 +4,6 @@ from ui import App
 def main():
     app = App()
     app.mainloop()
-    # args = parse_arguments()
-
-    # list_internationals = load_students(args.interxl, is_international=True)
-    # list_locals = load_students(args.localxl, is_international=False)
-
-    # print(
-    #     f"Successfully loaded {len(list_internationals)} international and {len(list_locals)} local students.\n"
-    # )
-
-    # matcher = Matcher(list_internationals, list_locals)
-    # df_top_matches = matcher.get_top_matches(top_n=args.topn)
-
-    # print("\n--- Results ---")
-    # print(df_top_matches.to_string(index=False))
-
-    # export_results_to_excel(df_top_matches, filename=args.output)
 
 
 if __name__ == "__main__":
